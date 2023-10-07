772b3025c7fba48d1edda8284500cd1555c43edb
Jaime Eduardo Ríos Villegas <142249529+jeriosv@users.noreply.github.com>
2023-10-06 22:26:51 -0500

    Update Reto6.8.py

--- Reto6.8.py
+++ Reto6.8.py
@@ -1,28 +1,28 @@
-def promedio(a,b,c,d,e): #cálculo de promedio
+def promedio(a,b,c,d,e): 
     return((a+b+c+d+e)/5)
 
-def mediana(a,b,c,d,e): #cálculo de mediana
+def mediana(a,b,c,d,e): 
     datos = [a,b,c,d,e]
     return (stats.mean(datos))
 
-def promediomultiplicativo(a,b,c,d,e): #cálculo de promedio multiplicativo
+def promedioMultiplicativo(a,b,c,d,e): 
     m=(a*b*c*d*e)
     return math.sqrt(m)
 
-def ordenascendente(a,b,c,d,e): #ordenamiento de números de forma ascendente
+def ordenAscendente(a,b,c,d,e): 
     lista = [a,b,c,d,e]
     lista.sort()
     return lista
 
-def ordenadescendente(a,b,c,d,e): #ordenamiento de números de forma descendente
+def ordenDescendente(a,b,c,d,e): 
     listap = [a,b,c,d,e]
     listap.sort(reverse=True)
     return listap
                 
-def potencia(a,b,c,d,e): #potencia del máximo número elevado al menor número
+def potencia(a,b,c,d,e): 
     listapb = [a,b,c,d,e]
     return(max(listapb)**min(listapb))
 
-def raizcubicamenor(a,b,c,d,e): #raíz cúbica del menor número
+def raizCubicaMenor(a,b,c,d,e): 
     listapbp = [a,b,c,d,e]
     return((min(listapbp))**(1/3))
