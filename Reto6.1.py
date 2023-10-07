d1dfcba371024565815f49acc48072fcca654808
Jaime Eduardo Ríos Villegas <142249529+jeriosv@users.noreply.github.com>
2023-10-06 22:17:24 -0500

    Update Reto6.1.py

--- Reto6.1.py
+++ Reto6.1.py
@@ -1,22 +1,22 @@
 import math
 
-def calcular_volumen(radio_esfera:float, radio_cono:float, altura_cono:float) -> float:
-    volumen_esfera = (4/3) * (radio_esfera**3) * math.pi
-    volumen_cono = (altura_cono/3) * (radio_cono**2) * math.pi
-    return volumen_esfera + volumen_cono    
+def calcularVolumen(radioEsfera:float, radioCono:float, alturaCono:float) -> float:
+   volumenEsfera = (4/3) * (radioEsfera**3) * math.pi
+   volumenCono = (alturaCono/3) * (radioCono**2) * math.pi
+   return volumenEsfera + volumenCono    
 
-def calcular_area(radio_esfera:float, radio_cono:float, altura_cono:float) -> float:
-    area_esfera = 4 * math.pi * radio_esfera**2
-    altura_oblicua = math.sqrt(altura_cono*2 + radio_cono*2)
-    area_cono = (math.pi * radio_cono * altura_oblicua) + (math.pi * radio_cono**2)
-    return area_esfera + area_cono
+def calcularArea(radioEsfera:float, radioCono:float, alturaCono:float) -> float:
+   areaEsfera = 4 * math.pi * radioEsfera**2
+   alturaOblicua = math.sqrt(alturaCono*2 + radioCono*2)
+   areaCono = (math.pi * radioCono * alturaOblicua) + (math.pi * radioCono**2)
+   return areaEsfera + areaCono
 
 if __name__ == '__main__':
-    radio_esfera = float(input("Ingrese el radio de la esfera en cm: "))
-    radio_cono = float(input("Ingrese el radio del cono en cm: "))
-    altura_cono = float(input("Ingrese la altura del cono en cm: "))
-    volumen = calcular_volumen(radio_esfera, radio_cono, altura_cono)
-    area = calcular_area(radio_esfera, radio_cono, altura_cono)
-    
-    print("El volumen de la figura es: " + str(volumen) + " cm^3")
-    print("El área de la figura es: " + str(area) + " cm^2")
+   radioEsfera = float(input("Ingrese el radio de la esfera en cm: "))
+   radioCono = float(input("Ingrese el radio del cono en cm: "))
+   alturaCono = float(input("Ingrese la altura del cono en cm: "))
+   volumen = calcularVolumen(radioEsfera, radioCono, alturaCono)
+   area = calcularArea(radioEsfera, radioCono, alturaCono)
+ 
+   print("El volumen de la figura es: " + str(volumen) + " cm^3")
+   print("El área de la figura es: " + str(area) + " cm^2")
