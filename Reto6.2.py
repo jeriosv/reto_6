ed65b578086d626f380bbd6d465e9e80a29ae5ef
Jaime Eduardo Ríos Villegas <142249529+jeriosv@users.noreply.github.com>
2023-10-06 22:18:59 -0500

    Update Reto6.2.py

--- Reto6.2.py
+++ Reto6.2.py
@@ -1,21 +1,21 @@
 import math
 
 def perimetro(r:float, a:float, b:float) -> float:
-    rectangulo = 2 * b + 2 * a
-    circulo = 2 * math.pi * r
-    return rectangulo + 2 * circulo
+  rectangulo = 2 * b + 2 * a
+  circulo = 2 * math.pi * r
+  return rectangulo + 2 * circulo
 
 def area(r:float, a:float, b:float) -> float:
-    rectangulo = a * b
-    circulo = math.pi * r**2
-    return rectangulo + 2 * circulo
+  rectangulo = a * b
+  circulo = math.pi * r**2
+  return rectangulo + 2 * circulo
 
-if __name__ == '__main__':
-    r = float(input("Ingrese el valor del radio (en cm): "))
-    a = float(input("Ingrese la longitud del lado más corto del rectángulo (en cm): "))
-    b = float(input("Ingrese la longitud del lado más largo del rectángulo (en cm): "))
-    per = perimetro(r, a, b)
-    ar = area(r, a, b)
-    
-    print("El perímetro de la figura es: " + str(per) + " cm.")
-    print("El área de la figura es: " + str(ar) + " cm².")
+if _name_ == '_main_':
+  r = float(input("Ingrese el valor del radio (en cm): "))
+  a = float(input("Ingrese la altura del rectángulo (en cm): "))
+  b = float(input("Ingrese la longitud del rectángulo (en cm): "))
+  per = perimetro(r, a, b)
+  ar = area(r, a, b)
+
+print("El perímetro de la figura es: " + str(per) + " cm.")
+print("El área de la figura es: " + str(ar) + " cm².")
