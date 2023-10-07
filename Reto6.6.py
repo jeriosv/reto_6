82475e9ce1c8d1d1c18b86e790faa7c741056bd4
Jaime Eduardo Ríos Villegas <142249529+jeriosv@users.noreply.github.com>
2023-10-06 22:25:25 -0500

    Update Reto6.6.py

--- Reto6.6.py
+++ Reto6.6.py
@@ -1,8 +1,8 @@
-def calcular_contagios(contagiados_actuales: int, dias_transcurridos: int) -> int:
-    return contagiados_actuales * (2 ** dias_transcurridos)
+def calcularContagios(contagiadosActuales: int, diasTranscurridos: int) -> int:
+  return contagiadosActuales * (2 ** diasTranscurridos)
 
 if __name__ == '__main__':
-    contagiados_actuales = int(input("Ingrese el número de contagiados actuales: "))
-    dias_transcurridos = int(input("Ingrese el número de días a partir de hoy: "))
-    total_contagios = calcular_contagios(contagiados_actuales, dias_transcurridos)
-    print(f"El número total de personas contagiadas después de {dias_transcurridos} días es: {total_contagios}")
+  contagiadosActuales = int(input("Ingrese el número de contagiados actuales: "))
+  diasTranscurridos = int(input("Ingrese el número de días transcurridos a partir de hoy: "))
+  totalContagios = calcularContagios(contagiadosActuales, diasTranscurridos)
+  print(f"El número total de personas contagiadas después de {diasTranscurridos} días es: {totalContagios}")
