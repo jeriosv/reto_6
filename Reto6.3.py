79144178d2fa8d116d8acad6cca30546c587b935
Jaime Eduardo Ríos Villegas <142249529+jeriosv@users.noreply.github.com>
2023-10-06 22:21:26 -0500

    Update Reto6.3.py

--- Reto6.3.py
+++ Reto6.3.py
@@ -1,14 +1,14 @@
-def calcular_cantidad_carne_aves():
-    peso_gallinas = n_gallinas * 6
-    peso_gallos = m_gallos * 7
-    peso_pollitos = k_pollitos * 1
-    total_peso = peso_gallinas + peso_gallos + peso_pollitos
-    return total_peso
+def calcularCantidadCarneAves():
+  pesoGallinas = nGallinas * 6
+  pesoGallos = mGallos * 7
+  pesoPollitos = kPollitos * 1
+  pesoTotal = pesoGallinas + pesoGallos + pesoPollitos
+  return pesoTotal
 
 if __name__ == '__main__':
-    n_gallinas = int(input("Ingrese la cantidad de gallinas: "))
-    m_gallos = int(input("Ingrese la cantidad de gallos: "))
-    k_pollitos = int(input("Ingrese la cantidad de pollitos: "))
-    cantidad_carne = calcular_cantidad_carne_aves()
-    
-    print("La cantidad de carne de aves es:", cantidad_carne, "kilos")
+  nGallinas = int(input("Ingrese el número de gallinas: "))
+  mGallos = int(input("Ingrese el número de gallos: "))
+  kPollitos = int(input("Ingrese el número de pollitos: "))
+  cantidadCarne = calcularCantidadCarneAves()
+
+  print("La cantidad de carne de aves es:", cantidadCarne, "kilos")
